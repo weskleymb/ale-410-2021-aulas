002c8f53308185a5eb28a3ef5d8b0ed470e0f187
Weskley Bezerra <weskleymb@hotmail.com>
2022-01-12 21:09:55 -0300

    aulas

--- AulaOrientacaoObjetos/src/tela/LoginConta.java
+++ AulaOrientacaoObjetos/src/tela/LoginConta.java
@@ -7,6 +7,28 @@ import java.util.Scanner;
 public class LoginConta {
 
     public void mostrarTela(Scanner entrada, ContaGenerica conta) {
+        mostrarOpcoes(conta);
+        Integer escolha = entrada.nextInt();
+
+        switch (escolha) {
+            case 1:
+                depositar(entrada, conta);
+                break;
+        }
+    }
+
+    private void depositar(Scanner entrada, ContaGenerica conta) {
+        System.out.println("Informe o valor a ser depositado:");
+        Double valor = entrada.nextDouble();
+
+        conta.depositar(valor);
+
+        System.out.println("Saldo atual: " + conta.getSaldo());
+
+        mostrarTela(entrada, conta);
+    }
+
+    private void mostrarOpcoes(ContaGenerica conta) {
         System.out.println("Bem vindo a sua conta " + conta.getTitular().getNome());
         System.out.println("+------------------------+");
         System.out.println("|-Operações da Conta-----|");
